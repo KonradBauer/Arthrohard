fecf1acc868cdfbe74d351d980ec72777eec4679
Konrad Bauer <konradbauer94@gmail.com>
2023-10-13 00:51:39 +0200

    added top ease tile hover effect

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,31 +1,18 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { fetchProducts } from "../../fetchProducts";
+import { ProductsContainer, Tile } from "./styled";
 
 export const GetProducts = () => {
   const { isLoading, isPaused, error, data } = useQuery(["products"], fetchProducts);
 
   if (data) {
     return (
-      <>
-        <h1>Products</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Product</th>
-              <th>Value</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.data.map((product) => (
-              <tr key={product.id}>
-                <td>{product.name}</td>
-                <td>{product.value}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </>
+      <ProductsContainer>
+        {data.data.map((product) => (
+          <Tile key={product.id}>ID: {product.id}</Tile>
+        ))}
+      </ProductsContainer>
     );
   }
 
---
+++ src/components/Products/styled.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const ProductsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+  padding: 143px;
+`;
+
+export const Tile = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 371px;
+  height: 350px;
+  padding: 155px;
+  gap: 10px;
+  background: #bebebe;
+  font-family: Futura PT;
+  font-size: 31px;
+  font-weight: 450;
+  line-height: 40px;
+  letter-spacing: 0em;
+  text-align: center;
+  margin-bottom: 35px;
+  position: relative;
+  top: 0;
+  transition: top ease 0.5s;
+
+  &:hover {
+    border: 1px solid #484848;
+    cursor: pointer;
+    top: -10px;
+  }
+`;
