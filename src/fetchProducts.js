03ca1304a38b5dcbc208131e91ea659d08445557
Konrad Bauer <konradbauer94@gmail.com>
2023-10-14 22:32:30 +0200

    fetchProducts is now handle by axios

--- src/fetchProducts.js
+++ src/fetchProducts.js
@@ -1,2 +1,10 @@
-export const fetchProducts = () =>
-  fetch("https://brandstestowy.smallhost.pl/api/random").then((response) => response.json());
+import axios from "axios";
+
+export const fetchProducts = async () => {
+  try {
+    const response = await axios.get("https://brandstestowy.smallhost.pl/api/random");
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to fetch products: " + error.message);
+  }
+};
