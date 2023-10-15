324d393bb88ca43f24437bf7c23cc3a28896610b
Konrad Bauer <konradbauer94@gmail.com>
2023-10-15 02:49:39 +0200

    react modal installed, create Modal component folders

---
+++ src/components/Modal/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Modal = styled.modal`
+  width: 843px;
+  height: 285px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background: #fff;
+  box-shadow: 0px 4px 16px 0px rgba(0, 0, 0, 0.25);
+`;
