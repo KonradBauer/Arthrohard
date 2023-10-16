e1d4e2a741725dd7a4d9fc242176a17531ae5bd9
Konrad Bauer <konradbauer94@gmail.com>
2023-10-16 18:36:06 +0200

    test modal deleted

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import { GetProducts } from "./components/Products";
 import { Main } from "./components/Main";
 import { Ingredients } from "./components/Ingredients";
 import { BrowserRouter } from "react-router-dom";
-import { Modal } from "./components/Modal";
 
 function App() {
   return (
     <BrowserRouter>
-      <Modal />
       <GlobalContainer>
         <Header />
         <Main />
