3cadad96bb42c6bc0adc8302dc3275a042591bb8
Konrad Bauer <konradbauer94@gmail.com>
2023-10-18 22:48:09 +0200

    added 0.3 sec to axios fetch for better see loader

--- src/fetchProducts.js
+++ src/fetchProducts.js
@@ -5,6 +5,9 @@ export const fetchProducts = async (pageNumber, pageSize) => {
     const response = await axios.get(
       `https://brandstestowy.smallhost.pl/api/random?pageNumber=${pageNumber}&pageSize=${pageSize}`
     );
+
+    await new Promise((resolve) => setTimeout(resolve, 300));
+
     return response.data;
   } catch (error) {
     throw new Error(`Error fetching products: ${error.message}`);
