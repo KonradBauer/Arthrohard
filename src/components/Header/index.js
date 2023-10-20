556df014d33d9c3e0de2bee862f616490cfedd7b
Konrad Bauer <konradbauer94@gmail.com>
2023-10-21 00:39:33 +0200

    adain added returns, because app doesnt work well

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,18 +2,20 @@ import React from "react";
 import { Content, Navigation, StyledLogo, StyledNavButtons } from "./styled";
 
 export const Header = () => {
-  <Content>
-    <StyledLogo />
-    <Navigation>
-      <StyledNavButtons onClick={() => (window.location.href = "#about")}>
-        Co nas wyróżnia
-      </StyledNavButtons>
-      <StyledNavButtons onClick={() => (window.location.href = "#ingredients")}>
-        Skład
-      </StyledNavButtons>
-      <StyledNavButtons onClick={() => (window.location.href = "#products")}>
-        <strong>Produkty</strong>
-      </StyledNavButtons>
-    </Navigation>
-  </Content>;
+  return (
+    <Content>
+      <StyledLogo />
+      <Navigation>
+        <StyledNavButtons onClick={() => (window.location.href = "#about")}>
+          Co nas wyróżnia
+        </StyledNavButtons>
+        <StyledNavButtons onClick={() => (window.location.href = "#ingredients")}>
+          Skład
+        </StyledNavButtons>
+        <StyledNavButtons onClick={() => (window.location.href = "#products")}>
+          <strong>Produkty</strong>
+        </StyledNavButtons>
+      </Navigation>
+    </Content>
+  );
 };
--- src/components/ProductPreview/index.js
+++ src/components/ProductPreview/index.js
@@ -10,19 +10,21 @@ import {
 } from "./styled";
 
 export const ProductPreview = () => {
-  <>
-    <Wrapper>
-      <StyledImage />
-      <ProductInfo>
-        <StyledLogo />
-        <Description>Lab-V Arthrohard Preparat na wsparcie stawów dla psa i kota</Description>
-        <SubDescription>
-          Arthrohard to dobrze przyswajalny suplement diety w formie syropu,
-          <br /> stworzony z myślą o zdrowiu stawów Twojego psa lub kota.
-        </SubDescription>
-        <BuyButton>Kup teraz</BuyButton>
-      </ProductInfo>
-    </Wrapper>
-    <StyledMainLeftVector />
-  </>;
+  return (
+    <>
+      <Wrapper>
+        <StyledImage />
+        <ProductInfo>
+          <StyledLogo />
+          <Description>Lab-V Arthrohard Preparat na wsparcie stawów dla psa i kota</Description>
+          <SubDescription>
+            Arthrohard to dobrze przyswajalny suplement diety w formie syropu,
+            <br /> stworzony z myślą o zdrowiu stawów Twojego psa lub kota.
+          </SubDescription>
+          <BuyButton>Kup teraz</BuyButton>
+        </ProductInfo>
+      </Wrapper>
+      <StyledMainLeftVector />
+    </>
+  );
 };
