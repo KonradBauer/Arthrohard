e3aeb52576aef6f47c55c217c73eb85cac97cd5d
Konrad Bauer <konradbauer94@gmail.com>
2023-10-20 18:58:09 +0200

    options in select name content change

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -65,10 +65,10 @@ export const GetProducts = () => {
           onChange={(event) => setItemsPerPage(parseInt(event.target.value, 10))}
           value={itemsPerPage}
         >
-          <option value="10">10 sztuk</option>
-          <option value="20">20 sztuk</option>
-          <option value="30">30 sztuk</option>
-          <option value="50">50 sztuk</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="30">30</option>
+          <option value="50">50</option>
         </Select>
       </SelectContainer>
       <ProductsContainer id="products">
