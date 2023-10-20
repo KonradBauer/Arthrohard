c63c9e9fc7be72dbacfb7fcf4f862dc591613bc5
Konrad Bauer <konradbauer94@gmail.com>
2023-10-20 22:37:14 +0200

    extracted ProductPreviev files from children Modal folder made by accident

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Header } from "./components/Header";
 import { GetProducts } from "./components/Products";
 import { Main } from "./components/Main";
 import { Ingredients } from "./components/Ingredients";
-import { ProductPreview } from "./components/ProductPreview/Modal";
+import { ProductPreview } from "./components/ProductPreview";
 
 function App() {
   return (
