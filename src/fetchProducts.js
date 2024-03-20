2b27535a7489a3d521e0208e249c01e1eec1ad65
Konrad Bauer <konradbauer94@gmail.com>
2024-03-20 21:44:36 +0100

    actualize fetchProducts API

--- src/fetchProducts.js
+++ src/fetchProducts.js
@@ -1,11 +1,16 @@
 import axios from "axios";
 
 export const fetchProducts = async (pageNumber, pageSize) => {
-  const response = await axios.get(
-    `https://brandstestowy.smallhost.pl/api/random?pageNumber=${pageNumber}&pageSize=${pageSize}`
-  );
+  try {
+    const response = await axios.get(
+      `https://picsum.photos/v2/list?page=${pageNumber}&limit=${pageSize}`
+    );
 
-  await new Promise((resolve) => setTimeout(resolve, 500));
+    await new Promise((resolve) => setTimeout(resolve, 500));
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    throw error;
+  }
 };
